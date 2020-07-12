21e2f48d07ce4654e01cd6ca37fbe68e8e02b92b
Mohamed Ayadi <48367135+AyadiGithub@users.noreply.github.com>
2020-07-12 13:41:41 -0400

    Add files via upload

---
+++ Image Colorizer/Extractor.py
@@ -0,0 +1,20 @@
+"""
+
+Extracting Images from Video frames.
+
+"""
+
+import cv2
+
+# Opens the Video file
+cap = cv2.VideoCapture('C:/Videos/Play.mp4')
+i=0
+while(cap.isOpened()):
+    ret, frame = cap.read()
+    if ret == False:
+        break
+    cv2.imwrite('kang'+str(i)+'.jpg',frame)
+    i+=1
+
+cap.release()
+cv2.destroyAllWindows()
